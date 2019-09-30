628a853c16380e161fad9b2e7af3e06cc80d3030
173917 <173917@IC0-W-N08359R.ssg.global>
2019-09-30 18:02:51 +0900

    싱크

--- smonWeb/src/main/java/com/shinsegae/smon/SmonWebApplication.java
+++ smonWeb/src/main/java/com/shinsegae/smon/SmonWebApplication.java
@@ -2,13 +2,12 @@ package com.shinsegae.smon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:${jdbc.config}" })
 //@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
-@ImportResource("classpath:config/context-aspect.xml")
+//@ImportResource("classpath:config/context-aspect.xml")
 public class SmonWebApplication {
 
 	public static void main(String[] args) {
