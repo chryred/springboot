8f51ab810c6eb53772a84a84c25938ac26dff59a
chryred <chryred@naver.com>
2019-10-03 14:44:53 +0900

    sync

--- smonWeb/src/main/java/com/shinsegae/smon/SmonWebApplication.java
+++ smonWeb/src/main/java/com/shinsegae/smon/SmonWebApplication.java
@@ -1,14 +1,13 @@
 package com.shinsegae.smon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(value = { "classpath:${jdbc.config}" })
 //@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
+//@ImportResource("classpath:config/context-aspect.xml")
 public class SmonWebApplication {
 
 	public static void main(String[] args) {
