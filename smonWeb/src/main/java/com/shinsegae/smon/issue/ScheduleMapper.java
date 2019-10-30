42a2a59e1d6e4ba5c1e091edef0507719d0991a6
Stephen <scha9393@gmail.com>
2019-10-30 13:31:40 +0900

    /* Schedule fix 차승욱 */

--- smonWeb/src/main/java/com/shinsegae/smon/issue/ScheduleMapper.java
+++ smonWeb/src/main/java/com/shinsegae/smon/issue/ScheduleMapper.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.SqlSessionTemplate;
 
 @Mapper
 public interface ScheduleMapper {
