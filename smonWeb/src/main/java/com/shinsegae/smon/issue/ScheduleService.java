bb952361f39c7558cc0fb909c7c4d2f6bdbce1e5
Stephen <scha9393@gmail.com>
2019-10-30 11:25:08 +0900

    /* schedule service annotation fix */

--- smonWeb/src/main/java/com/shinsegae/smon/issue/ScheduleService.java
+++ smonWeb/src/main/java/com/shinsegae/smon/issue/ScheduleService.java
@@ -12,8 +12,9 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-@Controller
+@Service
 public class ScheduleService {
 
 	@Autowired
