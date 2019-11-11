ad0b6f3057d5897a1e5f46949b9b69468f226a5e
184544 <184544@IC0-W-N08623R.ssg.global>
2019-11-11 11:20:04 +0900

    no23. Jenkins 연간현황 / no24. Jenkins 룰셋검출현황

---
+++ smonWeb/src/main/java/com/shinsegae/smon/security/code/JenkinsInfoMapper.java
@@ -0,0 +1,23 @@
+package com.shinsegae.smon.security.code;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface JenkinsInfoMapper {
+	
+	List<HashMap<String, Object>> selectTotalJobNm(Map<String, String> param) throws Exception;
+
+	List<HashMap<String, Object>> selectFixedCntByMonth(Map<String, String> param) throws Exception;
+
+	List<HashMap<String, Object>> selectJenkinsTotalInfo(Map<String, String> param) throws Exception;
+
+	List<HashMap<String, Object>> selectJenkinsJobInfo(Map<String, String> param) throws Exception;
+
+	List<HashMap<String, Object>> selectJobNmPivotColumns(Map<String, Object> param) throws Exception;
+
+	List<HashMap<String, Object>> selectJenkinsRuleSetInfoList(Map<String, Object> param) throws Exception;
+}
