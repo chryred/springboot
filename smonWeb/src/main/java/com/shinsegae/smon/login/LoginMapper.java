cd24b02ad3e928cfef113b992cd8aa529ff4c701
175582 <175582@IC0-W-N05212.ssg.global>
2019-11-12 14:08:23 +0900

    모델객체 추가

--- smonWeb/src/main/java/com/shinsegae/smon/login/LoginMapper.java
+++ smonWeb/src/main/java/com/shinsegae/smon/login/LoginMapper.java
@@ -1,5 +1,6 @@
 package com.shinsegae.smon.login;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -7,4 +8,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface LoginMapper {
 	Map<String, Object> searchUser(Map<String, Object> param);
+	HashMap<String, Object> searchUser2(HashMap<String, Object> param);
+	int newFaqReplyCount(String userId);
 }
--- smonWeb/src/main/java/com/shinsegae/smon/login/LoginService.java
+++ smonWeb/src/main/java/com/shinsegae/smon/login/LoginService.java
@@ -1,11 +1,14 @@
 package com.shinsegae.smon.login;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import com.shinsegae.smon.util.NLogger;
 
 @Service
 public class LoginService {
@@ -17,5 +20,17 @@ public class LoginService {
 	
 	public Map<String, Object> searchUser(Map<String, Object> map) {
 		return sqlSessionTemplatePrimary.getMapper(LoginMapper.class).searchUser(map);
+	}
+	
+	public HashMap<String, Object> oneUser(HashMap<String, Object> params) throws Exception{
+		NLogger.info("----- call oneNotice in NoticeDAO -----");
+		
+		return sqlSessionTemplatePrimary.getMapper(LoginMapper.class).searchUser2(params);
+	}
+	
+	public int newFaqReplyCount(String userId){
+		NLogger.info("----- call newFaqReplyCount in FaqDAO -----");
+		
+		return sqlSessionTemplatePrimary.getMapper(LoginMapper.class).newFaqReplyCount(userId);
 	}
 }
