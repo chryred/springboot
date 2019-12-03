e1ff66b625c5b435a036f4fa5e1cf70451c21444
173917 <173917@IC0-W-N08359R.ssg.global>
2019-12-03 16:05:57 +0900

    merge

--- smonWeb/src/main/java/com/shinsegae/smon/model/CubeoneVO.java
+++ smonWeb/src/main/java/com/shinsegae/smon/model/CubeoneVO.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-package com.shinsegae.smon.model;
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
-public class CubeoneVO {
-	private String status = "200";
-	private String msg;
-	private int crudLog;
-	private String itemCd;
-	private String tableName;
-	private String columnName;
-	private String errorMsg;
-	private String retMsg;
-	
-	public String toParamValue() {
-		StringBuffer sb = new StringBuffer();
-		
-		sb.append("msg=").append(msg)
-		  .append("&").append("crudLog=").append(crudLog)
-		  .append("&").append("itemCd=").append(itemCd)
-		  .append("&").append("tableName=").append(tableName)
-		  .append("&").append("columnName=").append(columnName);
-		
-		return sb.toString();
-	}
-}
-=======
 package com.shinsegae.smon.model;
 
 import lombok.Builder;
@@ -64,4 +31,4 @@ public class CubeoneVO {
 		return sb.toString();
 	}
 }
->>>>>>> 7c7b1fe5bf4110e7cd7a6ab4999068782d4d4c2c
+
